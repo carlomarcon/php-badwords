6a04f96c25a1eee134fcbedc65be47823b642ee0
carlomarcon <carlomarcon1998@gmail.com>
2023-12-11 14:36:04 +0100

    add: structure index.php

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
     <title>Document</title>
 </head>
 <body>
-    
+   <form action="" method="GET">
+   <label for="paragraph">Inserire paragrafo:</label>
+    <input type="text" id="paragraph" name="paragraph" >
+    <label for="">Inserire parola da censurare:</label>
+    <input type="text" id="word" name="word">
+   
+    <button type="submit">Invia</button>
+   </form>
 </body>
 </html>
