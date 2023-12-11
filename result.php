e72a453b6b706f0ffac80094a59330d97bb06fd6
carlomarcon <carlomarcon1998@gmail.com>
2023-12-11 14:53:06 +0100

    ADD: LOWERCASE IN RESULT.PHP

--- result.php
+++ result.php
@@ -2,7 +2,7 @@
 $paragraph= $_GET['paragraph'];
 $bannedWord= $_GET['word'];
 
-$result=str_replace($bannedWord,' *** ',$paragraph);
+$result=str_replace(strtolower($bannedWord),' *** ',strtolower($paragraph));
 echo $result
 
 ?>
